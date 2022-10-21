60b31805348ffbf6b7c2ce2ac408f3371a88379a
0925Daiki <82555115+0925Daiki@users.noreply.github.com>
2022-10-21 15:22:31 +0800

    DELETE実装

---
+++ delete.php
@@ -0,0 +1,18 @@
+<?php
+
+    include_once('./dbconnect.php');
+
+    $id = $_GET['id'];
+
+    // var_dump($id);
+
+    // SQL文を準備
+    $sql = "DELETE FROM records WHERE id = :id";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+
+    
+
+?>
